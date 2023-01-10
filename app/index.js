9b449d1ccbf67983f1a895850d4ccbf77dc12edb
masonMO25 <masonneale@gmail.com>
2023-01-09 20:47:06 -0600

    added index changes

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import bookRouter from "./book/routes.js";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello").json({ message: "hi" });
-});
+// middleware to parse incoming json (POST)
+app.use(express.json());
 
-app.listen(port);
+app.use("/api/books", bookRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
